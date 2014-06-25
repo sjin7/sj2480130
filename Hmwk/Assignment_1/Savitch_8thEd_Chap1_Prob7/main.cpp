9b3bf0b706a76568c830a37f608871681166748a
sjin <jsbb_7@hotmail.com>
2014-06-25 15:55:52 -0700

    Signed-off-by: sjin <jsbb_7@hotmail.com>

---
+++ Hmwk/Assignment_1/Savitch_8thEd_Chap1_Prob7/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File: Savitch_8th_Chap1_Prob7
+ * Author: Shen Jin
+ * Created on June 25, 2014, 2:31 PM
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+
+    for (int i=0; i<50; i++){
+        cout << "*";
+    }
+    cout << endl;
+    
+    cout << "   C C C                  S S S S           !!\n";
+    cout << " C       C              S         S         !!\n";
+    cout << "C
+            
+    cout << endl;
+    for (int i=0; i<50; i++){
+        cout << "*";
+    }
+    
+    return 0;
+}
+
