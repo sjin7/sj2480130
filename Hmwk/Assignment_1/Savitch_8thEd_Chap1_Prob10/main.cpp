1bc0c56e0e6f653a4e9150e0e2284b2a5d0cacdb
sjin7 <jsbb_7@hotmail.com>
2014-06-25 23:44:53 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

---
+++ Hmwk/Assignment_1/Savitch_8thEd_Chap1_Prob10/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File: Savitch_8thEd_Chap1_Prob10
+ * Author: Shen Jin
+ * Created on June 25, 2014, 11:09 PM
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+// Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+
+    char Lt;
+    
+    cout << "Type in a letter.\n";
+    cout << "Letter: ";
+    cin >> Lt;
+    cout << "    " << Lt << " " << Lt << " " << Lt << endl;
+    cout << "   " << Lt << "     " << Lt << endl;
+    cout << "  " << Lt << endl;
+    cout << "  " << Lt << endl;
+    cout << "  " << Lt << endl;
+    cout << "  " << Lt << endl;
+    cout << "  " << Lt << endl;
+    cout << "   " << Lt << "     " << Lt << endl;
+    cout << "    " << Lt << " " << Lt << " " << Lt << endl;
+    
+    return 0;
+}
+
