cd799bfca9322d11b3fcfaa6e764aee218e06a9e
sjin7 <jsbb_7@hotmail.com>
2014-06-28 16:02:37 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

---
+++ Hmwk/Assignment_2/Gaddis_7thEd_Chap3_Prob4/main.cpp
@@ -0,0 +1,59 @@
+/* 
+ * File:   Gaddis_7thEd_Chap3_Prob4
+ * Author: Shen Jin
+ * Created on June 28, 2014, 3:37 PM
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+// Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+
+    //Declare Variables
+    char M1, M2, M3;
+    float R1, R2, R3;
+    float avg;
+    
+    cout << "This program will calculate the average \n"
+            "rainfall for any three months.\n";
+    
+    cout << "First Month: "; 
+    cin >> M1;
+    cout << "Rainfall(in): ";
+    cin >> R1;
+    
+    cout << "------------------\n";
+    
+    cout << "Second Month: ";
+    cin >> M2;
+    cout << "Rainfall(in): ";
+    cin >> R2;
+    
+    cout << "------------------\n";
+    
+    cout << "Third Month: ";
+    cin >> M3;
+    cout << "Rainfall(in): ";
+    cin >> R2;
+    
+     cout << "------------------\n";
+     
+     //Calculation
+     avg = (R1 + R2 + R3) /3;
+     cout << "The average rainfall for " 
+          << M1 << ", " << M2 << " and " << M3
+          << " is " << avg << "." << endl;
+             
+    return 0;
+}
+
