0b67d9b1af71b2fb265fd6725f25d0bebaa72cc2
sjin7 <jsbb_7@hotmail.com>
2014-07-02 00:03:11 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

---
+++ Hmwk/Assignment_2/Gaddis_7thEd_Chap3_Prob17/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   Gaddis_7thEd_Chap3_Prob17
+ * Author: Shen Jin
+ * Created on July 1, 2014, 10:44 PM
+ */
+
+//System Libraries
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+
+// Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+
+    //Declare Variables
+    float LoanAmt, IntRate, Payment, Payback, IntPay;
+         
+    unsigned int N;
+    // N = number of payments
+    
+    //Calculation
+    IntRate*=()
+    Payment = 
+    
+    
+    return 0;
+}
+
