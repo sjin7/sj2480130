bd95eb384dd03c4cd791e344afc6b247506b9a08
sjin7 <jsbb_7@hotmail.com>
2014-07-02 11:03:52 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

--- Hmwk/Assignment_2/Gaddis_7thEd_Chap3_Prob17/main.cpp
+++ Hmwk/Assignment_2/Gaddis_7thEd_Chap3_Prob17/main.cpp
@@ -21,15 +21,36 @@ using namespace std;
 int main(int argc, char** argv) {
 
     //Declare Variables
-    float LoanAmt, IntRate, Payment, Payback, IntPay;
-         
-    unsigned int N;
-    // N = number of payments
+    //Input
+    float LoanAmt, IntRate;
+    
+    //Output
+    float Payment, Payback, IntPay;
+    unsigned int N;// N = number of payments
+    
+    //Prompt the user
+    cout << "What is the total loan amount?";
+    cin >> LoanAmt;
+    cout << "What is the monthly interest rate?";
+    cin >> IntRate;
+    cout << "How many payment?";
+    cin >> N;
     
     //Calculation
-    IntRate*=()
-    Payment = 
+    IntRate*=(1.0f/12/100); //Convert interest rate to percent/monthly
+    float temp = pow(1 + IntRate, N);
+    Payment = IntRate * temp * LoanAmt / (temp - 1);
+    Payback = Payment * N;
+    IntPay = Payback - LoanAmt;
     
+    //Set Decimal 
+    cout << fixed << showpoint << setprecision(2) << endl;
+    cout << "Loan Amount:           $" << LoanAmt << endl;
+    cout << "Monthly Interest Rate:  " << IntRate << endl;
+    cout << "Number of Payments:     " << N << endl;
+    cout << "Monthly Payment :      $" << Payment << endl;
+    cout << "Amount Paid Back:      $" << Payback << endl;
+    cout << "Interest Paid:         $" << IntPay << endl;
     
     return 0;
 }
