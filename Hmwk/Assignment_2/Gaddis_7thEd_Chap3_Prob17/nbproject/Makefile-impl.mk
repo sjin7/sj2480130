044bac4d0108c0a89bb553cbb9a84fc3c2bd5872
sjin7 <jsbb_7@hotmail.com>
2014-07-02 23:24:47 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

--- Hmwk/Assignment_2/Gaddis_7thEd_Chap3_Prob17/nbproject/Makefile-impl.mk
+++ Hmwk/Assignment_2/Gaddis_7thEd_Chap3_Prob17/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Gaddis_7thEd_Chap3_Prob17
+PROJECTNAME=gaddis_7thed_chap3_prob17
 
 # Active Configuration
 DEFAULTCONF=Debug
