f04275d7255dff9d32e2102bf1080400a1768a5c
sjin7 <jsbb_7@hotmail.com>
2014-07-07 01:52:25 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

---
+++ Hmwk/Assignment_3/Gaddis_7thEd_Chap4_Prob23/main.cpp
@@ -0,0 +1,80 @@
+/* 
+ * File:   Gaddis_7thEd_Chap4_Prob21
+ * Author: Shen Jin
+ * Created on July 7, 2014, 1:30 AM
+ */
+
+//System Library
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+//Rates for different packages
+float R1 = 9.95, R2 = 14.95, R3 = 19.95;
+//Rates for the "overtime" for the first two packages
+float r1 = 2.00, r2 = 1.00;
+//Hours included in first two packages
+int H1 = 10, H2 = 20;
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    
+    //Declare Input Variables
+    int hour; // Hours used in a month
+    char package; //type of package
+    
+    //Declare Output Variable
+    float cost; //Total cost of the month
+    
+    //Set decimal places for the outputs
+    cout << fixed << setprecision(2) << showpoint << endl;
+    
+    //Get the outputs
+    cout << "What type of package do you have?" << endl;
+    cin >> package;
+    cout << "How many hours did you use this month?" <<endl;
+    cin >> hour;
+
+    //Result
+    switch(package)
+    {
+        case 'A':
+            if (hour>=10 && hour <=774 )
+            {
+            cost = R1 + (hour - H1) * r1;
+            cout << "Cost: $" << cost << endl;
+            }
+            else 
+            {
+                cout << "Cost: $" << R1 << endl;
+            }
+            break;
+        case 'B':
+            if (hour>20 && hour <=774 )
+            {
+            cost = R2 + (hour - H2) * r2;
+            cout << "Cost: $" << cost << endl;
+            }
+            else
+            {
+                cout << "Cost: &" << R2 << endl;
+            }
+            break;
+        case 'C' :
+            cost = R3;
+            cout << "Cost: &" << R3 << endl;
+            break;
+        default:
+            cout << "Invalid Entry." << endl;
+            cout << "Cost: $" << cost << endl;
+    }
+    
+    return 0;
+}
+
