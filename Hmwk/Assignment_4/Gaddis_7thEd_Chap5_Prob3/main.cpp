9c39384d34ff2a85a8b2dd66b0b0871671c1c882
sjin7 <jsbb_7@hotmail.com>
2014-07-09 00:11:47 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

---
+++ Hmwk/Assignment_4/Gaddis_7thEd_Chap5_Prob3/main.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   Gaddis_7thEd_Chap5_Prob1
+ * Author: Shen Jin
+ * Created on July 8, 2014, 11:59 PM
+ */
+
+//System Library
+#include <iostream>
+
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants
+float r = 1.5; // The ocean level raises by 1.5ml/yr
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+
+    //Declare Variables
+    //Initialization
+    int stYr = 1; // Starting year
+    int edYr = 25; // Ending year
+    float totR = 0; // Total raise of the ocean level
+    int num; // Number of years
+    
+    cout << "Year             Ocean Level Increase(ml)" << endl;
+    cout << "-----------------------------------------" << endl;
+    
+    for (num=stYr; num<=edYr; num++)
+    {
+        totR +=r;
+        cout << num << "                  " << totR << endl;
+    }
+    
+    return 0;
+}
+
