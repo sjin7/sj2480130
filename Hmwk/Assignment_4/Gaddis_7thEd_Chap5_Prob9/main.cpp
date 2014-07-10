9442f548606202a9a15c3aab26fbcba3f8ad1d15
sjin7 <jsbb_7@hotmail.com>
2014-07-09 21:41:29 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

--- Hmwk/Assignment_4/Gaddis_7thEd_Chap5_Prob9/main.cpp
+++ Hmwk/Assignment_4/Gaddis_7thEd_Chap5_Prob9/main.cpp
@@ -7,7 +7,6 @@
 
 //System Library
 #include <iostream>
-#include <iomanip>
 
 
 using namespace std;
@@ -25,28 +24,39 @@ int main(int argc, char** argv) {
     int floor; //The floor number
     int numFl; //How many floors in this hotel
     int rooms; //Rooms this floor has in total
+    int totR; // Total rooms in this hotel
     int occu; //Rooms that are occupied
-//    int num; // Floor number used for initialization
+    int totO; //Total rooms occupied
     float percent; //The percentage of rooms that are occupied
     
     //Get the number of floors
     cout << "How many floors does this hotel have in total?" << endl;
     cin >> numFl;
     
-    for (floor=1; floor<=12; floor++)
+    
+    for (floor=1; floor<=numFl; floor++)
     {
-    //Get the number of rooms on each floor
-    cout << "How many rooms on this floor?" << endl;
-    cin >> rooms;
-    for (floor=14; floor<=numFl; floor++)
-    {
+        if(floor==13)
+        {
+            floor=14;
+        }
+        //Get the number of rooms on each floor
+        cout << "How many rooms on floor" << floor << "?" << endl;
+        cin >> rooms;
         //Get the number of rooms that are occupied
         cout << "How many rooms are occupied?" << endl;
         cin >> occu;
-        
-    }
+        totR += rooms;
+        totO += occu;
+     }
+     //Calculation for percentage of occupancy
+    percent = static_cast<float>(totO) / static_cast<float>(totR) * 100;
+        cout << "There are " << totR << " rooms, and "
+                << totO << " are occupied which is "  <<
+                percent << "%, therefore, there are "
+                << totR-totO << " rooms that are unoccupied." << endl;
     
-    }
+    
     
             return 0;
 }
