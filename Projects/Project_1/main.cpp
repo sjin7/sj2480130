5c9774546b1867c83abf4d13628f0b66a27845f7
sjin7 <jsbb_7@hotmail.com>
2014-07-14 12:33:20 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

---
+++ Projects/Project_1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on July 14, 2014, 10:37 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
