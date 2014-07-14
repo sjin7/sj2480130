203f0978e9ce12e8afc68f4740f16f22d0cfdc86
sjin7 <jsbb_7@hotmail.com>
2014-07-14 13:36:31 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

--- Projects/Project_1/main.cpp
+++ Projects/Project_1/main.cpp
@@ -1,19 +1,39 @@
 /* 
- * File:   main.cpp
- * Author: rcc
- *
+ * File:   Project 1 Roulette
+ * Author: Shen Jin
  * Created on July 14, 2014, 1:13 PM
  */
 
+//System Level Libraries
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
+//User Defined Libraries
 
-/*
- * 
- */
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
 int main(int argc, char** argv) {
 
+    //This is a program of Roulette
+    //Introduction of the game rules
+    cout << "Roulette is a casino game named after a French " << endl;
+    cout << "diminutive for little wheel. In the game, players " << endl;
+    cout << "may choose to place bets on either a single number " << endl;
+    cout << "or a range of numbers, the colors red or black, or " << endl;
+    cout << "whether the number is odd or even." << endl;
+    cout << "In this game, you are also allowed to bet on low, "<< endl;
+    cout << "medium, or high numbers." << endl;
+    
+    //Declare Variables
+    int num; //The number bet
+    char blk, red; //The color of the number bet
+    char low, med, high; //The range of the number bet
+    float bet; //The amount bet
+    
     return 0;
 }
 
