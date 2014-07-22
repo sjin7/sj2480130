48a6c21b95b0420c6a2a90e35532c96127e725bb
sjin7 <jsbb_7@hotmail.com>
2014-07-22 13:51:02 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

---
+++ Hmwk/Assignment_5/Gaddis_7thEd_Chap6_Prob3/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   Gaddis_7thEd_Chap6_Prob3
+ * Author: Shen Jin
+ * Created on July 22, 2014, 1:43 PM
+ */
+
+//System Level Libraries
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+//User Defined Libraries
+
+//Global Constants
+
+//Function Prototypes
+float getSales(float);
+void findHighest(float, char);
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
+float getSales(float norE, float souE, float norW, float souW){
+    
+}
