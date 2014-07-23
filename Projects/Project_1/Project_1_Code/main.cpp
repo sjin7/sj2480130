9ecad0242165dcee953a5a6c0403c03d5c3b0af1
sjin7 <jsbb_7@hotmail.com>
2014-07-22 22:00:28 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

--- Projects/Project_1/Project_1_Code/main.cpp
+++ Projects/Project_1/Project_1_Code/main.cpp
@@ -45,7 +45,7 @@ bool invalid;                   //For validation purpose
 //Set the random number seed
 srand(static_cast<unsigned short>(time(0)));   
 
-do{                                  //Would you like to play again/Input Validation  
+do{                              //Would you like to play again/Input Validation  
     cout << "How many times would you like to spin?" << endl;
     cin >> spin;
     for(int i=1; i<=spin; i++){     //Spin the wheel for times defined by user    
@@ -61,10 +61,11 @@ do{                                  //Would you like to play again/Input Valida
             //Determine the random number
             num=rand()%36+1;
 
+            //Start of switch case
             switch(type){
                 case 'A': 
                 case 'a':{      
-                    question(bank, bet, invalid);
+                    question(bank, bet, invalid);               //Use function
                     do{
                         invalid=false;
                         cout << "Which number would you like to bet from 1-36?" << endl;           
@@ -82,12 +83,12 @@ do{                                  //Would you like to play again/Input Valida
                     cout << setw(6) << num << setw(12) << numBet << endl;
                     cout << fixed << showpoint << setprecision(2) << endl;
                     if(numBet == num){             
-                        amnt = bank - bet + bet * 36;         //Calculation
+                        amnt = bank - bet + bet * 36;         //Calculation for win
                         cout << "Congratulations! You won $" << bet * 36 << endl; 
                         cout << "Now you have $" << amnt << endl << endl;
                     }
                     else{
-                        amnt = bank - bet;                    //Calculation
+                        amnt = bank - bet;                    //Calculation for lose
                         cout << "Sorry, you lost $" << bet << endl;
                         cout << "Now you have $" << amnt << endl;
                         cout << "Good luck on your next spin!" << endl << endl;
@@ -124,12 +125,12 @@ do{                                  //Would you like to play again/Input Valida
                     }
                     cout << fixed << showpoint << setprecision(2) << endl;
                     if((color[0]=='b' || color[0] =='B') && co==1){ 
-                        amnt = bank - bet + bet * 2;                    //Calculation
+                        amnt = bank - bet + bet * 2;                    //Calculation for win
                         cout << "Congratulations! You won $" << bet * 2 << endl;
                         cout << "Now you have $" << amnt<< endl;
                     }
                     else if((color[0]=='r' || color[0] =='R') && co==0){
-                        amnt = bank - bet + bet * 2;                    //Calculation
+                        amnt = bank - bet + bet * 2;                    //Calculation for lose
                         cout << "Congratulations! You won $" << bet * 2 << endl;
                         cout << "Now you have $" << amnt<< endl;
                     }
@@ -164,12 +165,12 @@ do{                                  //Would you like to play again/Input Valida
                     cout << setw(6) << num << setw(12) << choice << endl;
                     cout << fixed << showpoint << setprecision(2) << endl;
                     if(num%2==0 && (choice[0]=='E'||choice[0]== 'e')){ 
-                        amnt = bank - bet + bet * 2;                 //Calculation
+                        amnt = bank - bet + bet * 2;                 //Calculation for win
                         cout << "Congratulations! You won $" << bet * 2 << endl;
                         cout << "Now you have $" << amnt<< endl;
                     }          
                     else if(num%2==1 && (choice[0]=='O'||choice[0]=='o')){
-                        amnt = bank - bet + bet * 2;                //Calculation
+                        amnt = bank - bet + bet * 2;                //Calculation for lose
                         cout << "Congratulations! You won $" << bet * 2 << endl;
                         cout << "Now you have $" << amnt<< endl << endl;
                     }
@@ -204,17 +205,17 @@ do{                                  //Would you like to play again/Input Valida
                     cout << setw(6) << num << setw(12) << range << endl;
                     cout << fixed << showpoint << setprecision(2) << endl;
                     if((num>=1 && num<=12) && range[0]=='1'){
-                        amnt = bank - bet + bet * 3;                 //Calculation
+                        amnt = bank - bet + bet * 3;                 //Calculation for win
                         cout << "Congratulations! You won $" << bet * 3 << endl;
                         cout << "Now you have $" << amnt<< endl << endl;
                     }
                     else if((num>=13 && num<=24) && range[0]=='2'){
-                        amnt = bank - bet + bet * 3;                 //Calculation
+                        amnt = bank - bet + bet * 3;                 //Calculation for win
                         cout << "Congratulations! You won $" << bet * 3 << endl;
                         cout << "Now you have $" << amnt<< endl << endl;
                     }
                     else if((num>=25 && num<=36) && range[0]=='3'){
-                        amnt = bank - bet + bet * 3;                //Calculation
+                        amnt = bank - bet + bet * 3;                //Calculation for lose
                         cout << "Congratulations! You won $" << bet * 3 << endl;
                         cout << "Now you have $" << amnt<< endl << endl;
                     }
