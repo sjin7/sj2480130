96651f2693f3746159d374b3c7fb3d0fdb588b9f
sjin7 <jsbb_7@hotmail.com>
2014-07-26 16:05:00 -0700

    Signed-off-by: sjin7 <jsbb_7@hotmail.com>

---
+++ Projects/Project_2/Project_2_Code/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   Project_2_Roulette
+ * Author: Shen Jin
+ * Created on July 26, 2014, 4:02 PM
+ */
+
+//System Level Libraries
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+//User Defined Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+
+    //This roulette program will include the concepts of functions and array
+    
+    return 0;
+}
+
